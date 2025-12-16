ca2444fcfa16c6aa969e9a8bca6d3b547ab71508
Andre Faria <andre.faria.24@gmail.com>
2025-12-16 15:51:54 -0500

    added vpc_cidr output

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "vpc_arn" {
   value = module.vpc.vpc_arn
 }
 
+output "vpc_cidr" {
+  value = module.vpc.vpc_cidr_block
+}
+
 output "pvt_subnet_ids" {
   value = module.vpc.private_subnets
 }
